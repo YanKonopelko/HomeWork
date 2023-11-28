9a8f94e2517c9cda710f324f6df1374c6dab98d5
YanKonopelko <komand47@mail.ru>
2023-11-28 22:26:17 +0200

    CircleHit And MainMenu update

--- Assets/Scripts/CircleHit/InputManager.cs
+++ Assets/Scripts/CircleHit/InputManager.cs
@@ -1,22 +1,41 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace CircleHit
 {
     public class InputManager : MonoBehaviour
     {
         private PlayerMovement _player;
-
+        private int uiMask = 5;
+        [SerializeField] GraphicRaycaster m_Raycaster;
+        PointerEventData m_PointerEventData;
+        [SerializeField] EventSystem m_EventSystem;
         private void Start()
         {
             _player = FindObjectOfType<PlayerMovement>();
-
         }
 
         private void Update()
         {
-            if(_player is null) return;
-            if (Input.touchCount>0 && (Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetMouseButtonDown(0))
+            //Set up the new Pointer Event
+            m_PointerEventData = new PointerEventData(m_EventSystem);
+            //Set the Pointer Event Position to that of the game object
+            if(Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Began)
+                m_PointerEventData.position = Input.GetTouch(0).position;
+            else if(Input.GetMouseButtonDown(0))
+                m_PointerEventData.position = Input.mousePosition;
+            
+            m_PointerEventData.position = Input.mousePosition;
+            //Create a list of Raycast Results
+            List<RaycastResult> results = new List<RaycastResult>();
+ 
+            //Raycast using the Graphics Raycaster and mouse click position
+            m_Raycaster.Raycast(m_PointerEventData, results);
+ 
+            
+            if ((Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonDown(0)) && results.Count < 1)
             {
                 _player.Rotate(); 
             }
--- Assets/Scripts/Scenes/MainMenuScene.cs
+++ Assets/Scripts/Scenes/MainMenuScene.cs
@@ -1,22 +1,41 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScene : MonoBehaviour
 {
     [SerializeField] private string SettingsSceneName;
     [SerializeField] private string[] gameSceneNames;
+    [SerializeField] private GameObject CongratulationsScreen;
+    [SerializeField] private Text congratulationsText;
+
+    private string lastGame = "";
     private void Start()
     {
+        // PlayerPrefs.DeleteAll();
         //MusicManager.instance.Swap(MusicManager.MusicType.MainMenuMusic,3);
+        if (PlayerPrefs.GetInt("IsNewRecord") > 0)
+        {
+            congratulationsText.text = "Cool, you have a new record in " + PlayerPrefs.GetString("LastGame") + " game."+"\n "+PlayerPrefs.GetInt(PlayerPrefs.GetString("LastGame") + "BestScore");
+            CongratulationsScreen.SetActive(true);
+        }
+
     }
 
     public void StartGame(int SceneNum)
     {
         ScenesManager.instance.LoadScene(gameSceneNames[SceneNum]);
+        PlayerPrefs.SetString("LastGame",gameSceneNames[SceneNum]);
         //MusicManager.instance.Swap(MusicManager.MusicType.InGameMusic,3);
     }
     public void ToSettings()
     {
         ScenesManager.instance.LoadScene(SettingsSceneName);
+    }
+
+    public void CloseCongratulationsScreen()
+    {
+        CongratulationsScreen.SetActive(false);
+        PlayerPrefs.SetInt("IsNewRecord", 0);
     }
 }
